e7592dca6bc0ba75f6e7b4c6b9c955d6f62d6d5d
kirthiSaiT <kirthisai251@gmail.com>
2024-08-13 19:12:09 +0530

    added node modules

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const http=require("http");
 
-const port=8081;
+const port=3000;
 http.createServer((req,res)=>{
     res.writeHead(200,{"Content-Type":"text/html"});
     res.write("<h2>hey server started</h2>");
